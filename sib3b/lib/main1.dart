390a1c03d2646dd68485c907dccacf99476dda7a
dedelduwell <ardheliamputri@gmail.com>
2025-10-10 11:01:19 +0700

    elevated button

--- sib3b/lib/main1.dart
+++ sib3b/lib/main1.dart
@@ -36,7 +36,6 @@ class DellaApp extends StatelessWidget {
               ),
             ],
           ),
-          centerTitle: false,
           backgroundColor: const Color.fromARGB(255, 187, 51, 121),
           actions: const [
             Icon(Icons.search, color: Colors.white),
@@ -49,44 +48,19 @@ class DellaApp extends StatelessWidget {
         ),
         backgroundColor: const Color.fromARGB(255, 224, 164, 184),
         body: Container(
-          margin: const EdgeInsets.all(20),
-          padding: const EdgeInsets.all(16),
           alignment: Alignment.center,
-          //color: const Color.fromARGB(255, 60, 19, 32),
-          height: 200,
-          decoration: BoxDecoration(
-            gradient: const LinearGradient(
-              colors: [
-                Color.fromARGB(255, 187, 51, 121),
-                Color.fromARGB(255, 95, 12, 37),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-            //color: const Color.fromARGB(255, 95, 12, 37),
-            borderRadius: BorderRadius.horizontal(
-              left: Radius.circular(30),
-              right: Radius.circular(30),
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: const Color.fromARGB(255, 255, 254, 254),
-                spreadRadius: 2,
-                blurRadius: 8,
-                offset: const Offset(0, 4),
-              ),
-            ],
-          ),
+          color: const Color.fromARGB(255, 60, 19, 32),
+          height: 250,
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
-                Text(
+              children: [
+                const Text(
                   'You have pushed the button this many times:',
                   style: TextStyle(color: Colors.white),
                 ),
-                SizedBox(height: 8),
-                Row(
+                const SizedBox(height: 8),
+                const Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Icon(Icons.star, color: Colors.amber),
@@ -95,6 +69,17 @@ class DellaApp extends StatelessWidget {
                     Icon(Icons.star_half, color: Colors.amber),
                     Icon(Icons.star_border, color: Colors.amber),
                   ],
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Aksi ketika tombol ditekan
+                    debugPrint("Tombol di bawah Row ditekan!");
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 249, 219, 235),
+                  ),
+                  child: const Text('Press Me'),
                 ),
               ],
             ),
