b84541aac1b74ef77dc6a7c2d083e49dda7f814d
dedelduwell <ardheliamputri@gmail.com>
2025-10-10 12:28:19 +0700

    image

--- sib3b/lib/main1.dart
+++ sib3b/lib/main1.dart
@@ -14,29 +14,40 @@ class DellaApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 187, 51, 121),
           title: Row(
-            children: const [
-              Icon(Icons.account_circle, size: 40, color: Colors.white),
-              SizedBox(width: 12),
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                width: 40,
+                height: 40,
+              ),
+              //const Icon(Icons.account_circle, size: 40, color: Colors.white),
+              const SizedBox(width: 12),
               Expanded(
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
+                    const Text(
                       'Flutternya Della',
                       style: TextStyle(fontSize: 18, color: Colors.white),
                     ),
-                    Text(
+                    const Text(
                       'Della App',
                       style: TextStyle(fontSize: 12, color: Colors.white70),
                     ),
+                    const SizedBox(height: 6),
+                    // Image.asset(
+                    //   'assets/images/logo.png',
+                    //   width: 35,
+                    //   height: 35,
+                    // ),
                   ],
                 ),
               ),
             ],
           ),
-          backgroundColor: const Color.fromARGB(255, 187, 51, 121),
           actions: const [
             Icon(Icons.search, color: Colors.white),
             SizedBox(width: 10),
@@ -46,12 +57,13 @@ class DellaApp extends StatelessWidget {
             SizedBox(width: 8),
           ],
         ),
+
         backgroundColor: const Color.fromARGB(255, 224, 164, 184),
+
         body: Container(
           margin: const EdgeInsets.all(20),
           padding: const EdgeInsets.all(16),
           alignment: Alignment.center,
-          //color: const Color.fromARGB(255, 60, 19, 32),
           height: 250,
           decoration: BoxDecoration(
             gradient: const LinearGradient(
@@ -62,85 +74,86 @@ class DellaApp extends StatelessWidget {
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
-            //color: const Color.fromARGB(255, 95, 12, 37),
-            borderRadius: BorderRadius.horizontal(
+            borderRadius: const BorderRadius.horizontal(
               left: Radius.circular(30),
               right: Radius.circular(30),
             ),
-            boxShadow: [
+            boxShadow: const [
               BoxShadow(
-                color: const Color.fromARGB(255, 255, 254, 254),
+                color: Color.fromARGB(255, 255, 254, 254),
                 spreadRadius: 2,
                 blurRadius: 8,
-                offset: const Offset(0, 4),
+                offset: Offset(0, 4),
               ),
             ],
           ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'You have pushed the button this many times:',
-                  style: TextStyle(color: Colors.white),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'You have pushed the button this many times:',
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 8),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star, color: Colors.amber),
+                  Icon(Icons.star_half, color: Colors.amber),
+                  Icon(Icons.star_border, color: Colors.amber),
+                ],
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  debugPrint("Tombol di bawah Row ditekan!");
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 249, 219, 235),
                 ),
-                const SizedBox(height: 8),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.star, color: Colors.amber),
-                    Icon(Icons.star, color: Colors.amber),
-                    Icon(Icons.star, color: Colors.amber),
-                    Icon(Icons.star_half, color: Colors.amber),
-                    Icon(Icons.star_border, color: Colors.amber),
-                  ],
+                child: const Text('Press Me'),
+              ),
+              TextButton(
+                onPressed: () {
+                  debugPrint("TextButton ditekan!");
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
                 ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    // Aksi ketika tombol ditekan
-                    debugPrint("Tombol di bawah Row ditekan!");
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 249, 219, 235),
-                  ),
-                  child: const Text('Press Me'),
+                child: const Text('Text Button'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  debugPrint("OutlinedButton ditekan!");
+                },
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(color: Colors.white),
+                  foregroundColor: Colors.white,
                 ),
-                TextButton(
-                  onPressed: () {
-                    // Aksi ketika tombol ditekan
-                    debugPrint("TextButton ditekan!");
-                  },
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.white,
-                  ),
-                  child: const Text('Text Button'),
-                ),
-                OutlinedButton(
-                  onPressed: () {
-                    // Aksi ketika tombol ditekan
-                    debugPrint("OutlinedButton ditekan!");
-                  },
-                  style: OutlinedButton.styleFrom(
-                    side: const BorderSide(color: Colors.white),
-                    foregroundColor: Colors.white,
-                  ),
-                  child: const Text('Outlined Button'),
-                ),
-                
-              ],
-            ),
+                child: const Text('Outlined Button'),
+              ),
+              IconButton(
+                onPressed: () {
+                  debugPrint("IconButton ditekan!");
+                },
+                icon: const Icon(Icons.favorite, color: Colors.white),
+              ),
+            ],
           ),
         ),
+
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           tooltip: 'Increment',
           child: const Icon(Icons.add),
         ),
+
         drawer: Drawer(
           child: ListView(
             padding: EdgeInsets.zero,
-            children: const <Widget>[
+            children: const [
               DrawerHeader(
                 decoration: BoxDecoration(
                   color: Color.fromARGB(255, 187, 51, 121),
@@ -168,8 +181,9 @@ class DellaApp extends StatelessWidget {
             ],
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
+
+        bottomNavigationBar:  BottomNavigationBar(
+          items: [
             BottomNavigationBarItem(
               icon: Icon(Icons.home),
               label: 'Home',
@@ -187,5 +201,4 @@ class DellaApp extends StatelessWidget {
       ),
     );
   }
-  
 }
--- sib3b/test/widget_test.dart
+++ sib3b/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:sib3b/main.dart';
+import 'package:sib3b/main1.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( DellaApp());
+    await tester.pumpWidget(DellaApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
